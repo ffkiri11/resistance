c316f9754bce0c51a3800f6778d53a84592645b7
ff.kirill <redh4skell@gmail.com>
2023-04-24 16:32:15 +0000

    Switch to kyua test runner

--- Makefile
+++ Makefile
@@ -1,42 +1,6 @@
-scc = scalac -color never
-sc = scala
-src = src/main/scala/resistance
-jvm = java -jar
-classpath = $(PWD)/lib/binaries/scala-library.jar:$(PWD)/lib/binaries/scala3-library_3.jar:$(PWD)/cc2resistance.jar:$(PWD)/colorcode.jar
-ivy_zip = apache-ivy-2.5.0-bin.zip
-ivy_url = "https://archive.apache.org/dist/ant/ivy/2.5.0/$(ivy_zip)"
-ivy_jar = apache-ivy-2.5.0/ivy-2.5.0.jar 
+resistance : resistance.c resistance.h 
+	$(CC) resistance.c -o resistance -l
 
-export CLASSPATH := $(classpath)
+install : resistance
+	install resistance /usr/bin
 
-main : cc2resistance.jar colorcode.jar
-	echo \#!/bin/bash > resistance.sh
-	echo CLASSPATH=$(classpath) java resistance.ColorCodeToResistance >> resistance.sh
-
-clean :
-	rm *.jar
-
-$(ivy_zip) :  
-	wget $(ivy_url)
-
-ivy-jar : $(ivy_zip)
-	unzip -o $(ivy_zip) 
-
-resolve : ivy.xml ivy-jar $(ivy_jar)
-	$(jvm)  $(ivy-jar) -retrieve 'lib/[conf]/[artifact](-[classifier]).[ext]'
-
-colorcode.jar : $(src)/ColorCode.scala
-	$(scc) $(src)/ColorCode.scala -d colorcode.jar
-
-colorcodetest.jar : $(src)/ColorCodeTest.scala
-	$(scc) $(src)/ColorCodeTest.scala -d colorcodetest.jar
-	
-cc2resistance.jar : $(src)/ColorCodeToResistance.scala colorcode.jar
-	$(scc) $(src)/ColorCodeToResistance.scala -d cc2resistance.jar
-
-test : colorcode.jar colorcodetest.jar
-	echo $$CLASSPATH
-	CLASSPATH=$$CLASSPATH:colorcodetest.jar java resistance.ColorCodeTest
-
-shell:
-	scala
--- resistance.c
+++ resistance.c
@@ -4,164 +4,94 @@
 #include <math.h>
 #include "resistance.h"
 
-static const char *bandv[] = {BAND};
-static const unsigned int bandiv[] = {BAND_INDEX};
-static size_t bandc = sizeof bandv / sizeof bandv[0];
-
-static const char *name;
+static const char *colors[] = {BAND};
+static const unsigned int colors_index[] = {BAND_INDEX};
+static size_t bandc = sizeof colors / sizeof colors[0];
 static void usage();
 
-static void setname(const char *s)
-{
-  name = s;
-}
 
-static void fprintvt(FILE *stream, const char *s[])
-{
-  while (*++s != NULL)
-	fprintf(stream, ", %s", *s);
-}
-
-static void invalid_argument(const char *opt)
-{
-  printf("%s: invalid argument value -- ", name);
-  printf("'%s'.\nTry '%s -h' for more information.\n", opt, name);
-  exit(EXIT_FAILURE);
-}
-
-static const int parse_format(const char *opt)
-{
-  char nadd;
-
-  nadd = *opt++ - '2';
-  if (nadd > '\1' || *opt != 's') usage();
-  return (int)nadd;
-}
-
-static const int parseopt(const char *opt)
-{
-  if (*opt++ != '-') usage();
-
-  if (*opt == 'h') usage();
-
-  if (*opt++ != 'f') usage();
-
-  return parse_format(opt);
-}
-
-static const int parse_tol(const char *to) { return -1; };
-
-static const char *parse_temp(const char *te) { return "-"; };
-
-static int pstrcmp(const char **s1, const char **s2)
+static int
+_keycmp(const char **s1, const char **s2)
 {
   return strcmp(*s1, *s2);
 }
 
-/*@null@*/ /*@dependent@*/ static const char **parse_band(const char *key)
+/*
+ * Returns iterator to the ring's color number in the details table
+ */
+static const char **
+find_ring(const char *key)
 {
-  return bsearch(&key, bandv, bandc, sizeof bandv[0],
-	(int (*)(const void *, const void *)) pstrcmp);
+  return (const char**)bsearch(&key, colors, bandc, sizeof *colors,
+	(int (*)(const void *, const void *)) _keycmp);
 }
 
-static int parse_value(const char *s)
+/*
+ * Returns resistance digit from color code
+ */
+static int
+get_resistance_digit(const char *s)
 {
-  /*@dependent@*/ const char **band = parse_band(s);
-
-  if (band == NULL) invalid_argument(s);
-
-  return -FIRST_SIGNIFICANT_INDEX + (int)bandiv[band - bandv];
+	const char **ring = find_ring(s);
+	if (ring == NULL) {
+		printf("Band \"%s\" is not found\n", s);
+		usage();
+	}
+        else
+		return  -FIRST_SIGNIFICANT_INDEX 
+			+ (int)colors_index[find_ring(s) - colors];
 }
 
-static void process_bands(int nadd, const char *valv[])
+/*
+ * Parse colors vector, returning float resistance in ohms
+ */
+double
+pcolors(const char * const* value)
 {
-  int tolerance_permile = NO_RING_TOLERANCE;
-  int significant = 0;
-  int mul;
-  int npos;
-  int pw;
-  double r;
-
-  /* Initialize calculation */
-  mul = 10;
-  npos = 2 + nadd;
-
-  /* I don't use floats in this app */
-  while (nadd != 0) {
-	mul *= 10;
-	--nadd;
+  int base = 0, pw = 0;
+  while (strcmp(*value, "-m")) {
+	base *= 10;
+	base += get_resistance_digit(*value);
+	++value;
   }
 
-  /* Accumulate significant part digit by digit */
-  while (npos != 0) {
-	significant += mul * parse_value(*valv);
-	mul /= 10;
-	++valv;
-	--npos;
-  }
-
-  /* Parse ten power */
-  if (*valv != NULL)
-  	pw = parse_value(*valv++);
-  else
-	usage();
-
-  /* Print resistance */
-  r = significant * pow(10, pw);
-  printf("Resistance: %.3e\t", r);
-
-  /* TODO: Parse tolerance */
-  if (*valv != NULL)
-	tolerance_permile = parse_tol(*valv++);
-
-  printf("Sigma: %.3e\t", r * tolerance_permile / 1000);
-
-  /* TODO: Parse temperature coefficient */
-  if (*valv != NULL)
-	printf("Temperature coefficient: %s\n",	parse_temp(*valv++));
-  else
-	printf("\n");
-
-  if (*valv != NULL) invalid_argument(*valv);
+  value++;
+  pw = get_resistance_digit(*(value++));
+  return base * pow((double)10, (double)pw);
 }
 
-static void usage()
+/*
+ * Process colors vector printing resistance value
+ */	
+void 
+process_bands(const char **colors)
 {
-  const char **p;
-  fprintf(stderr, "Usage: %s OPTION BAND BAND BAND [BAND...]\n", name);
-  fprintf(stderr, "Converts EIA color code to resistance value.\n\n");
-  fprintf(stderr, "Options are:\n");
+  printf("Resistance: %.3e\n", pcolors(colors));
+}
+
+/*
+ * Print program usage reference
+ */
+static void
+usage()
+{
   fprintf(stderr,
-	"  -f2s - from two significant bands and multiplier\n");
-  fprintf(stderr,
-	"  -f3s - from three significant bands and multiplier\n\n");
-  fprintf(stderr,
-	"Bands shall be space separated, may be written as colors "
-	"or codes.\n");
-	p = bandv;
-  fprintf(stderr, "Colors and color codes are: %s", *p);
-  fprintvt(stderr, p);
-  fprintf(stderr, ".\n");
-  fprintf(stderr, "After values bands could be: ");
-  fprintf(stderr, "tolerance band (default is 20%%) ");
-  fprintf(stderr, "and temperature coefficient band.\n");
+	"Usage: resistance BAND BAND [BAND ...] -m BAND\n"
+	"Converts EIA color code to resistance value.\n");
   exit(EXIT_FAILURE);
 }
 
-int main(const int argc, const char *argv[])
+/*
+ * Program entry point
+ */
+int
+main(const int argc, const char *argv[])
 {
-  int nadd;
-
-  setname(*argv++);
-
-  if (argc < 5)
-	usage();
-
-  nadd = parseopt(*argv++);
-
-  if ((nadd > 1) || (argc < 5 + nadd))
-	usage();
-  process_bands(nadd, argv);
+  argv++;
+  if (argc > 4)
+	  process_bands(argv);
+  else 
+	  usage();
   return 0;
 }
 
